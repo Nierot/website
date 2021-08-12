d8d5e1f99381612d257f1e0884d405d5027be296
Nierot <git@nierot.com>
2021-08-12 15:25:20 +0200

    ✨ Alles

---
+++ src/App.svelte
@@ -0,0 +1,53 @@
+<script>
+	import Project from "./Project.svelte";
+	import projecten from './projecten.json'
+
+	let current = 0
+
+	const click = () => {
+		if (current === projecten.length - 1) {
+			current = 0
+		} else {
+			current++
+		}
+	}
+</script>
+
+<main>
+	<h1>Niels Rotmensen</h1>
+	<h2>ik maak websites ofzo</h2>
+
+	<button on:click={click}>
+		<i class="fas fa-arrow-right"></i>
+	</button>
+
+	<div id="projecten">
+		<Project {...projecten[current]}/>
+	</div>
+
+</main>
+
+<style>
+
+	main {
+		text-align: center;
+		padding: 1em;
+		height: 100%;
+	}
+
+	h1 {
+		font-weight: 100;
+	}
+
+	h2 {
+		font-weight: 100;
+	}
+
+	#projecten {
+		padding-top: 20px;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
---
+++ src/Project.svelte
@@ -0,0 +1,83 @@
+<script>
+    export let name
+    export let link
+    export let logo
+    export let picture
+    export let description
+    export let github
+    export let technologies
+    export let mobileOnly
+</script>
+
+<style>
+    main {
+        background-color: var(--primary);
+        color: white;
+        box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+        transition: 0.3s;
+        min-width: 50%;
+    }
+
+    main:hover {
+        box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+    }
+
+    .center {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .text {
+        padding-bottom: 16px;
+        max-width: 80%;
+    }
+
+    h1 {
+        font-weight: 100;
+    }
+    
+    p {
+        padding: 10px
+    }
+
+    img {
+        max-height: 20vw;
+    }
+
+</style>
+
+<main>
+    {#if logo !== undefined}
+        <img src={logo} alt={name}/>
+    {:else}
+        <h1>{name}</h1>
+        <img src={picture} alt={''} />
+    {/if}
+    <div class="center">
+        <div class="text">
+            <p>
+                <i class="fas fa-file" /> 
+                {description}
+            </p>
+            <p>
+                <i class="fas fa-memory" /> 
+                {technologies}
+            </p>
+            <div>
+                <a href={link}>
+                    <i class="fas fa-link"></i>
+                    {mobileOnly ? 'Site (mobile only)' : 'Site'}
+                </a>
+            </div>
+            <div>
+                <a href={github}>
+                    <i class="fab fa-github"></i>
+                    GitHub
+                </a>
+            </div>
+        </div>
+    </div>
+
+</main>
